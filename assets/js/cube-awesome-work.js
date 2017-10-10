d728d26e5419983dcb2721427e6dace5122b3c4b
Caesar <caesarshia@gmail.com>
2017-10-10 14:56:30 -0700

    tenth

--- assets/js/cube-awesome-work.js
+++ assets/js/cube-awesome-work.js
@@ -29,27 +29,27 @@
         displayType: 'fadeIn',
         displayTypeSpeed: 400,
 
-        // singlePage popup
+        // singlePage: popup,
         // singlePageDelegate: '.cbp-singlePage',
         // singlePageDeeplinking: true,
         // singlePageStickyNavigation: true,
         // singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
         // singlePageCallback: function(url, element) {
-        // //     // to update singlePage content use the following method: this.updateSinglePage(yourContent)
-        // //     var t = this;
+        //     // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+        //     var t = this;
 
-        // //     $.ajax({
-        // //             url: url,
-        // //             type: 'GET',
-        // //             dataType: 'html',
-        // //             timeout: 30000
-        // //         })
-        // //         .done(function(result) {
-        // //             t.updateSinglePage(result);
-        // //         })
-        // //         .fail(function() {
-        // //             t.updateSinglePage('AJAX Error! Please refresh the page!');
-        // //         });
+        //     $.ajax({
+        //             url: url,
+        //             type: 'GET',
+        //             dataType: 'html',
+        //             timeout: 30000
+        //         })
+        //         .done(function(result) {
+        //             t.updateSinglePage(result);
+        //         })
+        //         .fail(function() {
+        //             t.updateSinglePage('AJAX Error! Please refresh the page!');
+        //         });
         // },
     }
 );
